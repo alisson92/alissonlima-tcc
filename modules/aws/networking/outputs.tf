0a28e73d4bed1a4948da9f297a430f922b85fdad
Alisson Lima <112453034+alisson92@users.noreply.github.com>
2025-08-07 01:52:32 -0300

    Update outputs.tf

--- modules/aws/networking/outputs.tf
+++ modules/aws/networking/outputs.tf
@@ -1,11 +1,4 @@
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
-
-output "public_subnet_id" {
-  value = aws_subnet.public.id
-}
-
-output "private_subnet_id" {
-  value = aws_subnet.private.id
+output "vpc_cidr_block_output" {
+  description = "O bloco CIDR da VPC criada."
+  value       = aws_vpc.main.cidr_block
 }
