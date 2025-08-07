d82d3b5ea3f5145d62e7d70b218139772f6ebe81
Alisson Lima <112453034+alisson92@users.noreply.github.com>
2025-08-07 02:05:07 -0300

    Update main.tf

--- modules/aws/security/main.tf
+++ modules/aws/security/main.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "bastion" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["${var.my_ip}/32"]
+    cidr_blocks = [var.my_ip]
   }
 
   egress {
