cee444cc39861115482e0a355285ba7185acf3fc
Alisson Lima <112453034+alisson92@users.noreply.github.com>
2025-08-07 02:02:19 -0300

    Update outputs.tf

--- modules/aws/security/outputs.tf
+++ modules/aws/security/outputs.tf
@@ -2,9 +2,10 @@ output "sg_bastion_id" {
   value = aws_security_group.bastion.id
 }
 
-output "sg_alb_id" {
-  value = aws_security_group.alb.id
-}
+# Comente ou apague o bloco abaixo por enquanto
+# output "sg_alb_id" {
+#  value = aws_security_group.alb.id
+# }
 
 output "sg_app_id" {
   value = aws_security_group.app.id
