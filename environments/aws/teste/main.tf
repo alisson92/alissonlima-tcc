22d99c743ac3c24c6284c957babc55d3c4421a4b
Alisson Lima <112453034+alisson92@users.noreply.github.com>
2025-08-07 01:52:18 -0300

    Update main.tf

--- environments/aws/teste/main.tf
+++ environments/aws/teste/main.tf
@@ -13,19 +13,11 @@ module "networking" {
 module "security" {
   source = "../../../modules/aws/security"
 
-  vpc_id      = module.networking.vpc_id 
-  environment = "teste"
-  # --- CORREÇÃO APLICADA ABAIXO ---
-  my_ip       = "45.230.208.30/32" 
+  vpc_id         = module.networking.vpc_id 
+  # Passando o CIDR da VPC para o módulo de segurança
+  vpc_cidr_block = module.networking.vpc_cidr_block_output # Precisamos criar este output
+  environment    = "teste"
+  my_ip          = "45.230.208.30/32" 
 }
 
-module "app_environment_teste" {
-  source = "../../../modules/aws/app_environment"
-
-  environment       = "teste"
-  private_subnet_id = module.networking.private_subnet_id
-  sg_app_id         = module.security.sg_app_id
-  sg_db_id          = module.security.sg_db_id
-  ami_id            = "ami-da7a780751ae1b2435"
-  key_name          = "tcc-alisson-key"
-}
+# module "app_environment_teste" { ... } # Comentado por enquanto
