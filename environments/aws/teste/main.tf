ff45e7bb25a54bfef5931c0b4b6a6b0076177988
Alisson <alissoncorrealima@hotmail.com>
2025-08-07 01:22:15 -0300

    teste

--- environments/aws/teste/main.tf
+++ environments/aws/teste/main.tf
@@ -13,23 +13,18 @@ module "networking" {
 module "security" {
   source = "../../../modules/aws/security"
 
-  vpc_id      = module.networking.vpc_id
+  vpc_id      = module.networking.vpc_id 
   environment = "teste"
-  my_ip       = "45.230.208.30" # Lembre-se de manter seu IP correto aqui
+  my_ip       = "45.230.208.30" # Verifique se este valor está 100% correto
 }
 
-# --- NOVO BLOCO ---
-# Chama o módulo para criar o par de servidores do ambiente de teste
 module "app_environment_teste" {
   source = "../../../modules/aws/app_environment"
 
-  # Conectando as saídas dos outros módulos como entradas aqui
   environment       = "teste"
   private_subnet_id = module.networking.private_subnet_id
   sg_app_id         = module.security.sg_app_id
   sg_db_id          = module.security.sg_db_id
-
-  # Preencha com os valores que você pegou no Passo 1
-  ami_id   = "ami-0c55b159cbfafe1f0" # <-- SUBSTITUA PELO ID DA AMI DO UBUNTU 22.04 NA SUA REGIÃO
-  key_name = "tcc-alisson-key"       # <-- SUBSTITUA PELO NOME DO SEU PAR DE CHAVES
+  ami_id            = "ami-da7a780751ae1b2435" # Ubuntu 22.04 LTS para us-east-1 (x86)
+  key_name          = "tcc-alisson-key"      # Verifique se este é o nome exato do seu par de chaves
 }
