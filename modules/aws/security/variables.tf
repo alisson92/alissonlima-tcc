18fddb9cbda634fec8f1d18910a57452891c91b0
Alisson Lima <112453034+alisson92@users.noreply.github.com>
2025-08-07 01:55:49 -0300

    Update variables.tf

--- modules/aws/security/variables.tf
+++ modules/aws/security/variables.tf
@@ -1,4 +1,14 @@
-variable "vpc_cidr_block" {
-  description = "O bloco CIDR da VPC para usar nas regras de segurança."
+variable "vpc_id" {
+  description = "ID da VPC onde os Security Groups serão criados."
+  type        = string
+}
+
+variable "environment" {
+  description = "Nome do ambiente (ex: teste, homol, prod) para usar nas tags."
+  type        = string
+}
+
+variable "my_ip" {
+  description = "Seu endereço IP público para permitir acesso SSH ao Bastion Host."
   type        = string
 }
