d64ee03618263933f9dfdd89e512b143e2d1f6f5
Alisson Lima <112453034+alisson92@users.noreply.github.com>
2025-08-07 02:01:42 -0300

    Update outputs.tf

--- modules/aws/networking/outputs.tf
+++ modules/aws/networking/outputs.tf
@@ -1,3 +1,18 @@
+output "vpc_id" {
+  description = "O ID da VPC criada."
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_id" {
+  description = "O ID da sub-rede pública."
+  value       = aws_subnet.public.id
+}
+
+output "private_subnet_id" {
+  description = "O ID da sub-rede privada."
+  value       = aws_subnet.private.id
+}
+
 output "vpc_cidr_block_output" {
   description = "O bloco CIDR da VPC criada."
   value       = aws_vpc.main.cidr_block
