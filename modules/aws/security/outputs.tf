be7e61482ea9ad4d4970a0de4d938e43d7cde943
Alisson Lima <112453034+alisson92@users.noreply.github.com>
2025-08-08 11:19:15 -0300

    Update outputs.tf

--- modules/aws/security/outputs.tf
+++ modules/aws/security/outputs.tf
@@ -1,16 +1,14 @@
 output "sg_bastion_id" {
-  value = aws_security_group.bastion.id
+  description = "ID do Security Group do Bastion Host."
+  value       = aws_security_group.bastion.id
 }
 
-# Comente ou apague o bloco abaixo por enquanto
-# output "sg_alb_id" {
-#  value = aws_security_group.alb.id
-# }
-
-output "sg_app_id" {
-  value = aws_security_group.app.id
+output "sg_alb_id" {
+  description = "ID do Security Group do Application Load Balancer."
+  value       = aws_security_group.alb.id
 }
 
-output "sg_db_id" {
-  value = aws_security_group.db.id
+output "sg_application_id" {
+  description = "ID do Security Group unificado para a aplicacao (App e DB)."
+  value       = aws_security_group.application.id
 }
