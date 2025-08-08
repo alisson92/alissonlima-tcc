8a89ecbd49dd8615f96ff2b89550676d79fe69b9
Alisson Lima <112453034+alisson92@users.noreply.github.com>
2025-08-08 12:11:38 -0300

    Update outputs.tf

--- environments/aws/teste/outputs.tf
+++ environments/aws/teste/outputs.tf
@@ -1,16 +1,17 @@
 # Exibe o IP público do Bastion Host para facilitar a conexão SSH
 output "bastion_public_ip" {
   description = "IP Público do Bastion Host para acesso SSH."
-  value       = module.bastion_host_teste.bastion_public_ip
+  # Adiciona o [0] para acessar o primeiro (e único) item da lista do módulo
+  value       = module.bastion_host_teste[0].bastion_public_ip
 }
 
 # Exibe os IPs privados dos servidores para facilitar os testes de conexão interna
 output "app_server_private_ip" {
   description = "IP Privado do servidor de aplicação."
-  value       = module.app_environment_teste.app_server_private_ip
+  value       = module.app_environment_teste[0].app_server_private_ip
 }
 
 output "db_server_private_ip" {
   description = "IP Privado do servidor de banco de dados."
-  value       = module.app_environment_teste.db_server_private_ip
+  value       = module.app_environment_teste[0].db_server_private_ip
 }
