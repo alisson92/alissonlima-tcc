3a2d952a8e5ea2a18083b7275edc39a7fa2b80f0
Alisson Lima <112453034+alisson92@users.noreply.github.com>
2025-08-08 12:15:06 -0300

    Update outputs.tf

--- environments/aws/teste/outputs.tf
+++ environments/aws/teste/outputs.tf
@@ -1,17 +1,17 @@
 # Exibe o IP público do Bastion Host para facilitar a conexão SSH
 output "bastion_public_ip" {
   description = "IP Público do Bastion Host para acesso SSH."
-  # Adiciona o [0] para acessar o primeiro (e único) item da lista do módulo
-  value       = module.bastion_host_teste[0].bastion_public_ip
+  # Lógica condicional: se o ambiente foi criado, mostra o IP. Senão, mostra "N/A".
+  value       = var.create_environment ? module.bastion_host_teste[0].bastion_public_ip : "N/A"
 }
 
 # Exibe os IPs privados dos servidores para facilitar os testes de conexão interna
 output "app_server_private_ip" {
   description = "IP Privado do servidor de aplicação."
-  value       = module.app_environment_teste[0].app_server_private_ip
+  value       = var.create_environment ? module.app_environment_teste[0].app_server_private_ip : "N/A"
 }
 
 output "db_server_private_ip" {
   description = "IP Privado do servidor de banco de dados."
-  value       = module.app_environment_teste[0].db_server_private_ip
+  value       = var.create_environment ? module.app_environment_teste[0].db_server_private_ip : "N/A"
 }
