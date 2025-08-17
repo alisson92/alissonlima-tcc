dffcefc3facbc39c03f9b4729c4925e59deae0e4
Alisson Lima <alisson.lima@newm.com.br>
2025-08-16 23:59:10 -0300

    ajuste fino

--- environments/aws/homol/outputs.tf
+++ environments/aws/homol/outputs.tf
@@ -1,18 +1,18 @@
 # Exibe o IP público do Bastion Host para facilitar a conexão SSH
 output "bastion_public_ip" {
   description = "IP Público do Bastion Host para acesso SSH."
-  value       = var.create_environment ? module.bastion_host_teste[0].bastion_public_ip : "N/A (ambiente destruído)"
+  value       = var.create_environment ? module.bastion_host[0].bastion_public_ip : "N/A (ambiente destruído)"
 }
 
 # Exibe os IPs privados dos servidores para facilitar os testes de conexão interna
 output "app_server_private_ip" {
   description = "IP Privado do servidor de aplicação."
-  value       = var.create_environment ? module.app_environment_teste[0].app_server_private_ip : "N/A (ambiente destruído)"
+  value       = var.create_environment ? module.app_environment[0].app_server_private_ip : "N/A (ambiente destruído)"
 }
 
 output "db_server_private_ip" {
   description = "IP Privado do servidor de banco de dados."
-  value       = var.create_environment ? module.app_environment_teste[0].db_server_private_ip : "N/A (ambiente destruído)"
+  value       = var.create_environment ? module.app_environment[0].db_server_private_ip : "N/A (ambiente destruído)"
 }
 
 # --- LINHA ADICIONADA/CORRIGIDA ---
