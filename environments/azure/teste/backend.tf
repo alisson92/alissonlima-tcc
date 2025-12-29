08967c99117a282d36a2df0477e3df438b454e3e
Alisson Lima <alisson.lima@newm.com.br>
2025-12-29 07:14:15 -0300

    fix

--- environments/azure/teste/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-terraform-state" # Crie este RG manualmente antes
-    storage_account_name = "alissonlimatcctfstate"  # Nome único global
-    container_name       = "tfstate"
-    key                  = "environments/azure/teste/terraform.tfstate"
-  }
-}
--- environments/azure/teste/versions.tf
+++ environments/azure/teste/versions.tf
@@ -1,13 +1,29 @@
 terraform {
   required_version = ">= 1.0.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "alissonlimatcctfstate"
+    container_name       = "tfstate"
+    key                  = "environments/azure/teste/terraform.tfstate"
   }
 }
 
 provider "azurerm" {
-  features {} # Requisito obrigatório do provider Azure
+  features {}
+}
+
+provider "aws" {
+  region = "us-east-1"
 }
