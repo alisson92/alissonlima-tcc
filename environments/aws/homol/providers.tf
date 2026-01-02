4d88d911b4726933e8a3c08c708b67c252529e64
Alisson Lima <alisson.lima@newm.com.br>
2026-01-02 03:18:45 -0300

    Ajustando/preparando ambiente de homologação

---
+++ environments/aws/homol/providers.tf
@@ -0,0 +1,14 @@
+# =====================================================================
+# ENVIRONMENTS/HOMOL/PROVIDERS.TF - PROVEDORES DE SERVIÇO
+# =====================================================================
+
+# Configuração do Provedor AWS
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Configuração do Provedor Cloudflare (Novo!)
+# Isso permitirá que o Terraform atualize seu DNS automaticamente
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
