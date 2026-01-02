c3340cb76f624d8ce64c8de6c2b241b42e1985ea
Alisson Lima <alisson.lima@newm.com.br>
2026-01-01 21:03:14 -0300

    Ajustando pipeline

---
+++ environments/aws/teste/dns.tf
@@ -0,0 +1,21 @@
+# =====================================================================
+# ENVIRONMENTS/TESTE/DNS.TF - AUTOMAÇÃO DE APONTAMENTOS CLOUDFLARE
+# =====================================================================
+
+# 1. Registro do Site (teste-aws.alissonlima.dev.br)
+resource "cloudflare_record" "app_aws" {
+  zone_id = var.cloudflare_zone_id
+  name    = "teste-aws"
+  value   = module.load_balancer[0].alb_dns_name # Pega dinamicamente do ALB
+  type    = "CNAME"
+  proxied = true # Nuvem Laranja (SSL + Proteção)
+}
+
+# 2. Registro do Bastion (bastion-teste.alissonlima.dev.br)
+resource "cloudflare_record" "bastion_aws" {
+  zone_id = var.cloudflare_zone_id
+  name    = "bastion-teste"
+  value   = module.bastion_host[0].bastion_public_ip # Pega dinamicamente o IP
+  type    = "A"
+  proxied = false # Nuvem Cinza (Obrigatório para SSH funcionar no PuTTY)
+}
