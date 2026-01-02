e6f48fcbe8186f340c2e37b752ea3db28556968a
Alisson Lima <alisson.lima@newm.com.br>
2026-01-01 21:23:53 -0300

    Fix

--- environments/aws/teste/dns.tf
+++ environments/aws/teste/dns.tf
@@ -1,21 +1,57 @@
 # =====================================================================
-# ENVIRONMENTS/TESTE/DNS.TF - AUTOMAÇÃO DE APONTAMENTOS CLOUDFLARE
+# DNS.TF - AUTOMAÇÃO DE DNS PÚBLICO (CLOUDFLARE) E PRIVADO (ROUTE 53)
 # =====================================================================
 
-# 1. Registro do Site (teste-aws.alissonlima.dev.br)
+# --- 1. DNS PÚBLICO (CLOUDFLARE) ---
+
 resource "cloudflare_record" "app_aws" {
+  # Garante que o registro só exista se o ambiente for criado
+  count   = var.create_environment ? 1 : 0 
+  
   zone_id = var.cloudflare_zone_id
   name    = "teste-aws"
-  content   = module.load_balancer[0].alb_dns_name # Pega dinamicamente do ALB
+  content = module.load_balancer[0].alb_dns_name
   type    = "CNAME"
-  proxied = true # Nuvem Laranja (SSL + Proteção)
+  proxied = true
 }
 
-# 2. Registro do Bastion (bastion-teste.alissonlima.dev.br)
 resource "cloudflare_record" "bastion_aws" {
+  count   = var.create_environment ? 1 : 0
+
   zone_id = var.cloudflare_zone_id
   name    = "bastion-teste"
-  content   = module.bastion_host[0].bastion_public_ip # Pega dinamicamente o IP
+  content = module.bastion_host[0].bastion_public_ip
   type    = "A"
-  proxied = false # Nuvem Cinza (Obrigatório para SSH funcionar no PuTTY)
+  proxied = false
+}
+
+# --- 2. DNS PRIVADO (ROUTE 53 - RESOLUÇÃO INTERNA DINÂMICA) ---
+
+resource "aws_route53_zone" "internal" {
+  count = var.create_environment ? 1 : 0
+  
+  name  = "internal.alissonlima.dev.br"
+  vpc {
+    vpc_id = module.networking.vpc_id
+  }
+}
+
+resource "aws_route53_record" "db_internal" {
+  count   = var.create_environment ? 1 : 0
+
+  zone_id = aws_route53_zone.internal[0].zone_id
+  name    = "db-server"
+  type    = "A"
+  ttl     = "300"
+  records = [module.db_server[0].db_server_private_ip]
+}
+
+resource "aws_route53_record" "app_internal" {
+  count   = var.create_environment ? 1 : 0
+
+  zone_id = aws_route53_zone.internal[0].zone_id
+  name    = "app-server"
+  type    = "A"
+  ttl     = "300"
+  records = [module.app_server[0].app_server_private_ips[0]]
 }
