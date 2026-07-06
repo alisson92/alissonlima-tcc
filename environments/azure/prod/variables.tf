5bd05d8a59588a2e07eac5c38057f9a827affcda
Alisson Lima <alissoncorrealima@hotmail.com>
2026-07-06 14:25:03 -0300

    fix(azure): correct environment_name default from teste to prod
    Same copy-paste bug as homol: default never adjusted after copying from
    teste/variables.tf.

--- environments/azure/prod/variables.tf
+++ environments/azure/prod/variables.tf
@@ -17,7 +17,7 @@ variable "create_environment" {
 variable "environment_name" {
   description = "O nome do ambiente (ex: teste, homol, prod)."
   type        = string
-  default     = "teste" # Valor padrão para agilizar o deploy
+  default     = "prod" # Valor padrão para agilizar o deploy
 }
 
 variable "vnet_cidr_block" {
